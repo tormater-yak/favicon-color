2c58fb65ddf483891d865778f08e0ad4d27ad2eb
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2025-01-08 11:47:50 -0800

    Better colored favicons

--- extension.php
+++ extension.php
@@ -1,15 +1,15 @@
 <?php
 function faviconColor($args) {
-    global $config, $grTop, $grMedium, $grBottom, $grHighlight, $grBorder;
+    global $config, $grTop, $grMedium, $grBottom, $grBottomDarker, $grHighlight, $grBorder;
     if (!is_null($config["forumColor"]) && isset($grMedium)) {
         $favicon_svg = file_get_contents("themes/Skyline/icon.svg");
-        $favicon_svg = str_replace("#0065c1", $grBottom, $favicon_svg);
-        $favicon_svg = str_replace("#219fff", $grMedium, $favicon_svg);
+        $favicon_svg = str_replace("#0065c1", hexAdjustLight($grMedium,-0.35), $favicon_svg);
+        $favicon_svg = str_replace("#219fff", $grTop, $favicon_svg);
         
         $favicon_svg = str_replace("#e1eeff", $grHighlight, $favicon_svg);
-        $favicon_svg = str_replace("#00afff", $grTop, $favicon_svg);
+        $favicon_svg = str_replace("#00afff", hexAdjustLight($grHighlight,-0.1), $favicon_svg);
         
-        $favicon_svg = str_replace("#0f488f", $grBorder, $favicon_svg);        
+        $favicon_svg = str_replace("#0f488f", $grBottomDarker, $favicon_svg);        
         
         $args[1]["favicon_svg"] = "data:image/svg+xml," . rawurlencode($favicon_svg);
     }
