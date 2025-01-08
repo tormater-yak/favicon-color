194397bc7462394fcab8f84129ba3e6b2a46fd95
Tormater Yak <115832947+tormater-yak@users.noreply.github.com>
2025-01-08 12:01:56 -0800

    Make favicons match Aurora better

--- extension.php
+++ extension.php
@@ -1,15 +1,10 @@
 <?php
 function faviconColor($args) {
-    global $config, $grTop, $grMedium, $grBottom, $grBottomDarker, $grHighlight, $grBorder;
-    if (!is_null($config["forumColor"]) && isset($grMedium)) {
-        $favicon_svg = file_get_contents("themes/Skyline/icon.svg");
-        $favicon_svg = str_replace("#0065c1", hexAdjustLight($grMedium,-0.35), $favicon_svg);
-        $favicon_svg = str_replace("#219fff", $grTop, $favicon_svg);
-        
-        $favicon_svg = str_replace("#e1eeff", $grHighlight, $favicon_svg);
-        $favicon_svg = str_replace("#00afff", hexAdjustLight($grHighlight,-0.1), $favicon_svg);
-        
-        $favicon_svg = str_replace("#0f488f", $grBottomDarker, $favicon_svg);        
+    global $config, $grBottomDarker;
+    if ($args[0] == "templates/header/header.html" && !is_null($config["forumColor"])) {
+        $favicon_svg = file_get_contents("extensions/" . basename(__DIR__) . "/favicon.svg");
+        $favicon_svg = str_replace("#00ff00", $config["forumColor"], $favicon_svg); 
+        $favicon_svg = str_replace("#ff0000", $grBottomDarker, $favicon_svg);     
         
         $args[1]["favicon_svg"] = "data:image/svg+xml," . rawurlencode($favicon_svg);
     }
